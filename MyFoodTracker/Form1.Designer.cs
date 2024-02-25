3562bd9e8be7f26db232a9857eecef8049f3abee
Leila Noval <109560883+lenovalsha@users.noreply.github.com>
2024-02-24 21:18:58 -0600

    Add project files.

---
+++ MyFoodTracker/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace MyFoodTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dtDisplayToDGV = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnToday = new System.Windows.Forms.Button();
+            this.grpFood = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.cmbMeal = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.txtFood = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.grpFood.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnDisplay.Location = new System.Drawing.Point(370, 198);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 28;
+            this.btnDisplay.Text = "Display All";
+            this.btnDisplay.UseVisualStyleBackColor = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSearch.Location = new System.Drawing.Point(307, 198);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(57, 22);
+            this.btnSearch.TabIndex = 27;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // dtDisplayToDGV
+            // 
+            this.dtDisplayToDGV.Location = new System.Drawing.Point(110, 198);
+            this.dtDisplayToDGV.Name = "dtDisplayToDGV";
+            this.dtDisplayToDGV.Size = new System.Drawing.Size(191, 20);
+            this.dtDisplayToDGV.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(74, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Date";
+            // 
+            // btnToday
+            // 
+            this.btnToday.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnToday.Location = new System.Drawing.Point(-29, 195);
+            this.btnToday.Name = "btnToday";
+            this.btnToday.Size = new System.Drawing.Size(97, 23);
+            this.btnToday.TabIndex = 24;
+            this.btnToday.Text = "Today";
+            this.btnToday.UseVisualStyleBackColor = false;
+            // 
+            // grpFood
+            // 
+            this.grpFood.Controls.Add(this.btnDelete);
+            this.grpFood.Controls.Add(this.btnUpdate);
+            this.grpFood.Controls.Add(this.cmbMeal);
+            this.grpFood.Controls.Add(this.label2);
+            this.grpFood.Controls.Add(this.btnCreate);
+            this.grpFood.Controls.Add(this.dtpDate);
+            this.grpFood.Controls.Add(this.lblDate);
+            this.grpFood.Controls.Add(this.txtFood);
+            this.grpFood.Controls.Add(this.label1);
+            this.grpFood.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpFood.Location = new System.Drawing.Point(29, 8);
+            this.grpFood.Name = "grpFood";
+            this.grpFood.Size = new System.Drawing.Size(867, 181);
+            this.grpFood.TabIndex = 23;
+            this.grpFood.TabStop = false;
+            this.grpFood.Text = "groupBox1";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnDelete.Location = new System.Drawing.Point(786, 152);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(81, 29);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnUpdate.Location = new System.Drawing.Point(649, 152);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(131, 29);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "UPDATE FOOD";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // cmbMeal
+            // 
+            this.cmbMeal.FormattingEnabled = true;
+            this.cmbMeal.Location = new System.Drawing.Point(689, 49);
+            this.cmbMeal.Name = "cmbMeal";
+            this.cmbMeal.Size = new System.Drawing.Size(121, 27);
+            this.cmbMeal.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(652, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Meal";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.AutoSize = true;
+            this.btnCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnCreate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCreate.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(224, 107);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(335, 34);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "ADD FOOD";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(433, 49);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 27);
+            this.dtpDate.TabIndex = 3;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDate.Location = new System.Drawing.Point(397, 52);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(39, 19);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "Date";
+            // 
+            // txtFood
+            // 
+            this.txtFood.Location = new System.Drawing.Point(43, 49);
+            this.txtFood.Name = "txtFood";
+            this.txtFood.Size = new System.Drawing.Size(348, 27);
+            this.txtFood.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Food";
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToOrderColumns = true;
+            this.dgvData.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvData.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvData.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Yi Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Cursor = System.Windows.Forms.Cursors.Hand;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Yellow;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvData.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvData.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvData.Location = new System.Drawing.Point(0, 8);
+            this.dgvData.MultiSelect = false;
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvData.Size = new System.Drawing.Size(1098, 476);
+            this.dgvData.TabIndex = 22;
+            this.dgvData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 484);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dtDisplayToDGV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnToday);
+            this.Controls.Add(this.grpFood);
+            this.Controls.Add(this.dgvData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpFood.ResumeLayout(false);
+            this.grpFood.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DateTimePicker dtDisplayToDGV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnToday;
+        private System.Windows.Forms.GroupBox grpFood;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ComboBox cmbMeal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox txtFood;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvData;
+    }
+}
+
